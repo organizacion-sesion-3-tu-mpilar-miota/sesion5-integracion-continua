6cbb254beddffc80f464b72b60d44f395b570f89
Pilar Miota <pilimiota@gmail.com>
2022-05-05 22:57:57 +0200

    Primer commit numeroPar

---
+++ ejercicios/numeroPar.py
@@ -0,0 +1,8 @@
+# En principio el alumno solo recibiría la definición de la función en el archivo correspondiente
+def numeroPar(x):
+
+    if x % 2 == 0:
+        return true
+    else
+        return false
+
---
+++ tests/test_numPar.py
@@ -0,0 +1,13 @@
+# Librería pytest para ejecutar tests
+import pytest
+
+# Importar archivo de ejercicio
+from ejercicios.numeroPar import numeroPar
+
+# Clase para crear tests. Las funciones de testeo deberán crearse en esta clase
+class TestClass:
+
+    # Test para la operación suma
+    def test_numpar(self):
+        assert numeroPar(2) == true
+        
